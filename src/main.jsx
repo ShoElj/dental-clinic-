fb2ad49d35264ab6221ea4de14c289eeee406542
shoElj <shodipoelijah2006@gmail.com>
2024-06-29 11:40:34 +0100

    Added concurrentely in backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import App from './App';
-import './index.css'; // Import Tailwind CSS
+import './index.css'
 
-ReactDOM.render(
+// Create a root container
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
