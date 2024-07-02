90237f53ba2e17b5619dfee78d1169c04f34798c
shoElj <shodipoelijah2006@gmail.com>
2024-07-02 16:23:44 +0100

    added few style changes

--- src/Components/Dashboard/SuperAdminDashboard.jsx
+++ src/Components/Dashboard/SuperAdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, TabPanel } from 'react-tabs';
+import { Tabs, TabPanel, TabList, Tab } from 'react-tabs';
 import MedicalRecords from '../Records/MedicalRecords';
 import CreateProfile from '../Profiles/CreateProfile';
 import MedicalHistory from '../History/MedicalHistory';
@@ -11,6 +11,15 @@ const SuperAdminDashboard = ({ selectedTab, onSelectTab }) => {
       <h1 className="text-2xl font-bold mb-4">Super Admin Dashboard</h1>
       <div className="flex-grow p-4">
         <Tabs selectedIndex={selectedTab} onSelect={index => onSelectTab(index)}>
+          <TabList>
+            <Tab>Dashboard</Tab>
+            <Tab>Create a medical record</Tab>
+            <Tab>Access Medical Records</Tab>
+            <Tab>Create Profile</Tab>
+            <Tab>Access Medical History</Tab>
+            <Tab>Logout</Tab>
+          </TabList>
+          
           <TabPanel>
             <MedicalRecords create={true} />
           </TabPanel>
